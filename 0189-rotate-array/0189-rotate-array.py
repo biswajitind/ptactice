17cfc89a7271e97ef112c328853bce03f3506299
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2025-12-31 18:14:31 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,16 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        l = len(nums)
-        k = k % l
-        def reverse_list(n, s, e):
-            while s < e:
-                n[s], n[e] = n[e], n[s]
-                s += 1
-                e -= 1
-        
-        reverse_list(nums, 0, l - 1)
-        reverse_list(nums, 0, k - 1)
-        reverse_list(nums, k, l-1 )
 
-        
+
+        def reverse_list(input, start, end):
+            while start < end:
+                input[start], input[end] = input[end], input[start]
+                start += 1
+                end -= 1
+
+        k = k % l        
+        reverse_list(nums, 0, len(nums) - 1)
+        reverse_list(nums, 0, k - 1)
+        reverse_list(nums, k, len(nums) - 1)
