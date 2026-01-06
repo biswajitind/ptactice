4186d89ccbfb175c3a41e53fc27255af3b033382
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-01-06 00:07:37 -0700

    Time: 12 ms (60.85%), Space: 20.8 MB (63.02%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # sort the words and match them
+        d = {}
+        for word in strs:
+            s = ''.join(sorted(word))
+            if s in d:
+                d[s].append(word)
+            else:
+                d[s] = [word]
+        return(list(d.values()))
