bf61614030e7e2f7fe24f20bbdc9ca348b7bf776
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-01-10 11:27:31 -0700

    Time: 0 ms (100%), Space: 17.7 MB (54.03%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -8,17 +8,36 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
 
-        def _inorderTraversal(root):
+        # Recursion
+        # def _inorderTraversal(root):
 
-            # Base case.
-            if not root:
-                return(0)
+        #     # Base case.
+        #     if not root:
+        #         return(0)
 
-            _inorderTraversal(root.left)
-            result.append(root.val)
-            _inorderTraversal(root.right)
+        #     _inorderTraversal(root.left)
+        #     result.append(root.val)
+        #     _inorderTraversal(root.right)
         
-        _inorderTraversal(root)
+        # _inorderTraversal(root)
+
+
+        # Iterative
+        stack = []
+        cur = root
+
+        while True:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            
+            if stack:
+                p = stack.pop()
+                result.append(p.val)
+                cur = p.right
+            else:
+                break
+
         return(result)
 
             
