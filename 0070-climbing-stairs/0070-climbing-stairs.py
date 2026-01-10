4cabc220cde15d556727233f74c8083e4a2e6b77
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-01-10 12:55:02 -0700

    Time: 0 ms (100%), Space: 17.6 MB (95.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -6,14 +6,22 @@ class Solution:
             2: 2
         }
 
-        def _ways(x: int) -> int:
-            if x in cache:
-                return(cache[x])
+        # def _ways(x: int) -> int:
+        #     if x in cache:
+        #         return(cache[x])
 
-            x_ways = _ways(x-1) + _ways(x-2)
-            cache[x] = x_ways
-            return(x_ways)
+        #     x_ways = _ways(x-1) + _ways(x-2)
+        #     cache[x] = x_ways
+        #     return(x_ways)
         
-        return(_ways(n))
+        # return(_ways(n))
+
+        if n in cache:
+            return(cache[n])
+
+        for i in range(3, n+1):
+            cache[i] = cache[i-1] + cache[i-2]
+ 
+        return(cache[n])
 
         
