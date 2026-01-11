ed25a9064a5af60a7b3ad4fda43937c533c3b9f3
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-01-10 22:59:19 -0700

    Time: 11 ms (16.75%), Space: 20.2 MB (7.38%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+
+        # With XOR
         # res = 0
         # for n in nums:
         #     res = res ^ n
         # return(res)
 
-        # with hashSet
+
+        # with hashSet, which will have 
         hs = set()
         for n in nums:
             if n in hs:
