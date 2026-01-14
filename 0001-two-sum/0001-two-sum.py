b8c3ed01c0a9b894c3b8e49e22263543d5e70d33
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-01-13 22:44:49 -0700

    Time: 0 ms (100%), Space: 13.2 MB (60.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,9 +6,16 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        # Brute Force
-        for i in range(len(nums) -1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return(i, j)
+        # Brute Force O(n**2) O(1)
+        # for i in range(len(nums) -1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return(i, j)
         
+        # with HashMap
+        cache = {}
+        for i in range(len(nums)):
+            if target - nums[i] in cache:
+                return([cache[target - nums[i]], i])
+            else:
+                cache[nums[i]] = i
