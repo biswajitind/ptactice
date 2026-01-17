37e59d449d936e7248fa2bd0f8fcc9ab5d4a211d
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-01-17 12:33:11 -0700

    Time: 22 ms (15.54%), Space: 18.1 MB (28.6%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,47 +1,22 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        res = ''
+
         if numRows <= 1:
             return(s)
+
+
+        for r in range(numRows):
+            inc = (numRows - 1) * 2
+            for i in range(r, len(s), inc):
+                res += s[i]
+                if r > 0 and r < numRows - 1 and i + inc - (2 * r) < len(s):
+                     res += s[i + inc - (2 * r)]
+
+        return(res)
             
-        n = len(s)
-        numCols = len(s)
-        matrix = [[None for _ in range(numCols+1)] for _ in range(numRows+1)]
-        
-
-        # My intuition is to build a matrix, with fixed number of rows,
-        # as grow in columns as ling as needed till we finish the Characters in the 
-        # input string. 
-        r, c = 0,0 
-        rInc, cInc = 1,0
-        for ch in s:
-            
-            matrix[r][c] = ch
-            if r+rInc >= numRows:
-                rInc = -1
-                cInc = 1
-            if r+rInc < 0:
-                rInc = 1
-                cInc = 0
-            r += rInc
-            c += cInc
-        
-        result = ''        
-        for i in range(numRows):
-            for j in range(numCols):
-                if matrix[i][j]:
-                    result = f'{result}{matrix[i][j]}'
-        return(result)
 
 
 
-
-        
-
-
-
-
-
-
-
-
+    
         
