5a43319dc2a2ed234b5df39a7dce391269b55c72
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-02-07 10:49:35 -0800

    Time: 1 ms (10.01%), Space: 18.9 MB (5.93%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,21 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        # Edge case Note:
-        # Since minimum number of elements in the list is 1 as per the Constrains, 
-        # there is no need to ckeck for Null or Empty list as input.
 
-        left = 0
-        right = len(nums) - 1
+        # Edge case
+        l = 0 
+        r = len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return(mid)
-            elif nums[mid] < target:
-                left = mid + 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return(m)
+            elif nums[m] < target:
+                l = m + 1
             else:
-                right = mid - 1
-
-        
+                r = m - 1
         return(-1)
+
+
+
