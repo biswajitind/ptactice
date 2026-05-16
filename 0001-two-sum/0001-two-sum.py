8f294d3a14030b00a39094a005f04fa7213750d0
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-05-16 10:33:35 -0800

    Time: 1 ms (59.7%), Space: 20.7 MB (14.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        # Brute Force O(n**2) O(1)
-        # for i in range(len(nums) -1):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return(i, j)
-        
-        # with HashMap
-        cache = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hMap = {}
         for i in range(len(nums)):
-            if target - nums[i] in cache:
-                return([cache[target - nums[i]], i])
+            if target - nums[i] in hMap:
+                return(hMap[target - nums[i]], i)
             else:
-                cache[nums[i]] = i
+                hMap[nums[i]] = i
+        
