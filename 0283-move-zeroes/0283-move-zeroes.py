0d5e3ca01d4ca615463f7ead141c7b09af8465e7
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-06-07 15:34:26 -0700

    Time: 0 ms (100%), Space: 20.4 MB (86.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,7 +5,20 @@ class Solution:
         """
         insert = 0
         for i in range(len(nums)):
-            if nums[i] != 0:
-                if insert != i:
+            if nums[i] == 0:
+                if nums[insert] == 0:
+                    #do nothing
+                    pass
+                else:
+                    insert = i
+            else:
+                if nums[insert] == 0:
+                    #swap
                     nums[insert], nums[i] = nums[i], nums[insert]
-                insert += 1
+                    insert += 1
+
+                
+
+            
+
+
