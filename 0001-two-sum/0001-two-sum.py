8a38c2ea521ce484bdd547b16b075e3e2216b15e
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-06-24 09:47:22 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Solution 1. Time O(N^2) Space O(1)
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return([i,j])
+        # for i in range(len(nums) - 1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return([i,j])
 
+        # Solution 2. Time O(N) Space O(N)
+        hMap = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in hMap:
+                return([hMap[target - nums[i]], i])
+            
+            hMap[nums[i]] = i
