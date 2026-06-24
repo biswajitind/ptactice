615506e0e440c08d9bea80ba7131bdcec0a51e5f
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-06-24 09:43:28 -0700

    Time: 1770 ms (11.68%), Space: 20 MB (72.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # for i in range(len(nums) - 1):
-        #     for j in range(i+1, len(nums)):
-        #         if (nums[i] + nums[j] == target):
-        #             return([i, j])
-        # return([])
-        hMap = {}
-        for i in range(len(nums)):
-            if (target - nums[i]) in hMap:
-                return([hMap[target - nums[i]], i])
-            else:
-                hMap[nums[i]] = i
+        # Solution 1. Time O(N^2) Space O(1)
+        for i in range(len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return([i,j])
 
