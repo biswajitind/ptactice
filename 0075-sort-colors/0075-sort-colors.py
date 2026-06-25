31b9742aff5fb44128761f1d0563b603e3f6ac30
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-06-24 22:17:10 -0700

    Time: 0 ms (100%), Space: 19.3 MB (78.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,27 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # # We will use bucket sort. one iteration to populate the bucket.
-        # bucket = {}
-        # for color in nums:
-        #     bucket[color] = bucket.get(color, 0) + 1
-        # idx = 0
-        # for i in range(0, 3):
-        #     for j in range(0, bucket.get(i, 0)):
-        #         nums[idx] = i
-        #         idx += 1
+        # We will use bucket sort. one iteration to populate the bucket.
+        bucket = {}
+        for color in nums:
+            bucket[color] = bucket.get(color, 0) + 1
+        idx = 0
+        for i in range(0, 3):
+            for j in range(0, bucket.get(i, 0)):
+                nums[idx] = i
+                idx += 1
 
-        # One pass, two pointers
-        l, r = 0, len(nums) - 1
-        i = 0 
-        while i <= r:
-            if nums[i] == 2:
-                nums[i], nums[r] = nums[r], nums[i]
-                r -= 1
-                continue
+        # # One pass, two pointers
+        # l, r = 0, len(nums) - 1
+        # i = 0 
+        # while i <= r:
+        #     if nums[i] == 2:
+        #         nums[i], nums[r] = nums[r], nums[i]
+        #         r -= 1
+        #         continue
 
-            if nums[i] == 0:
-                nums[i], nums[l] = nums[l], nums[i]
-                l += 1
-            i += 1
+        #     if nums[i] == 0:
+        #         nums[i], nums[l] = nums[l], nums[i]
+        #         l += 1
+        #     i += 1
 
