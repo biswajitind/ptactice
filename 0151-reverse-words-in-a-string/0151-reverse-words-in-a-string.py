498bdf207a2cbde97e3cf9ae0daa7d79d5107463
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-07-08 15:15:50 -0700

    Time: 3 ms (34.38%), Space: 19.2 MB (97.42%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,27 +1,22 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        wCount = 0
-        wList = []
         word = ""
+        result = ""
 
         for ch in s:
             if ch == " ":
                 if word:
-                    wList.append(word)
+                    if result:
+                        result = word + " " + result
+                    else:
+                        result = word
                     word = ""
             else:
                 word += ch
         if word:
-            wList.append(word)
-        print(wList)
-
-        # Now swap using two pointers
-        left = 0
-        right = len(wList) - 1
-
-        while left < right:
-            wList[left], wList[right] = wList[right], wList[left]
-            left += 1
-            right -= 1
+            if result:
+                result = word + " " + result
+            else:
+                result = word
         
-        return(" ".join(wList))
+        return(result)
